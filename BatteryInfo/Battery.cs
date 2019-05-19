df5003cd93f7903894aa6fa8f414c5240637bf6e
Sowa705 <sowa705@gmail.com>
2019-05-19 14:06:03 +0200

    Added error checking

--- BatteryInfo/Battery.cs
+++ BatteryInfo/Battery.cs
@@ -19,29 +19,37 @@ namespace BatteryInfo
         public float ChargeRate;
         public void Update()
         {
-            if (!(bool)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Active"))
+            try
+            {
+                if (!(bool)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Active"))
+                {
+                    Status = BatteryStatus.Unavailable;
+                    return;
+                }
+                Status = BatteryStatus.Discharging;
+                if ((bool)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Charging"))
+                    Status = BatteryStatus.Charging;
+
+                NominalCapacity = ((uint)WMITool.QuerySingle("root/WMI", "BatteryStaticData", "DesignedCapacity")) / 1000f;
+                RealCapacity = ((uint)WMITool.QuerySingle("root/WMI", "BatteryFullChargedCapacity", "FullChargedCapacity")) / 1000f;
+                CurrentCapacity = ((uint)WMITool.QuerySingle("root/WMI", "BatteryStatus", "RemainingCapacity")) / 1000f;
+                ChargePercent = CurrentCapacity / RealCapacity * 100f;
+
+                CurrentVoltage = ((uint)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Voltage")) / 1000f;
+                TimeToDischarge = TimeSpan.FromSeconds((uint)WMITool.QuerySingle("root/WMI", "BatteryRuntime", "EstimatedRuntime"));
+                if (Status == BatteryStatus.Discharging)
+                {
+                    ChargeRate = -((int)WMITool.QuerySingle("root/WMI", "BatteryStatus", "DischargeRate")) / 1000f;
+                }
+                else
+                {
+                    ChargeRate = ((int)WMITool.QuerySingle("root/WMI", "BatteryStatus", "ChargeRate")) / 1000f;
+                }
+            }
+            catch
             {
                 Status = BatteryStatus.Unavailable;
                 return;
-            }
-            Status = BatteryStatus.Discharging;
-            if ((bool)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Charging"))
-                Status = BatteryStatus.Charging;
-
-            NominalCapacity = ((uint) WMITool.QuerySingle("root/WMI","BatteryStaticData","DesignedCapacity"))/1000f;
-            RealCapacity = ((uint)WMITool.QuerySingle("root/WMI", "BatteryFullChargedCapacity", "FullChargedCapacity")) / 1000f;
-            CurrentCapacity = ((uint)WMITool.QuerySingle("root/WMI", "BatteryStatus", "RemainingCapacity")) / 1000f;
-            ChargePercent = CurrentCapacity / RealCapacity * 100f;
-
-            CurrentVoltage= ((uint)WMITool.QuerySingle("root/WMI", "BatteryStatus", "Voltage")) / 1000f;
-            TimeToDischarge = TimeSpan.FromSeconds((uint)WMITool.QuerySingle("root/WMI", "BatteryRuntime", "EstimatedRuntime"));
-            if (Status==BatteryStatus.Discharging)
-            {
-                ChargeRate = -((int)WMITool.QuerySingle("root/WMI", "BatteryStatus", "DischargeRate")) / 1000f;
-            }
-            else
-            {
-                ChargeRate = ((int)WMITool.QuerySingle("root/WMI", "BatteryStatus", "ChargeRate")) / 1000f;
             }
         }
     }
